d4649f287667699d138f8c8b82a2770b44ae302d
69PercentFat <shubham11501@gmail.com>
2023-12-15 19:29:24 +0530

    updated ui

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ function intialize() {
     // Create the game board
     for (let r = 0; r < height; r++) {
         for (let c = 0; c < width; c++) {
-            // <span id="0-0" class="tile">P</span>
             let tile = document.createElement("span");
             tile.id = r.toString() + "-" + c.toString();
             tile.classList.add("tile");
